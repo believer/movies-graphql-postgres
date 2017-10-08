2ecd1f8bbb19980d014dd291df236acf2db5bbdd
Rickard Laurin <rickard.laurin@gmail.com>
2017-10-08 16:44:06 +0200

    Add columns to users migration

--- migrations/20171008122726_users.js
+++ migrations/20171008122726_users.js
@@ -7,6 +7,8 @@ exports.up = (knex, Promise) => {
       .string('password')
       .unique()
       .notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
   })
 }
 
